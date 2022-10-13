76bf8660168accd83fdb4b5f7435a062a1c47bcf
Jiayi Nie <nie.jiay@northeastern.edu>
2022-10-12 17:18:33 -0700

    add explore screen post tweet counts

--- public/tuiter/PostSummaryList/PostSummaryItem.js
+++ public/tuiter/PostSummaryList/PostSummaryItem.js
@@ -1,5 +1,5 @@
 const PostSummaryItem = (post) => {
-    const {topic, userName, time, title, image} = post
+    const {topic, userName, time, title, image, tweets} = post
     return (`
         <li class="list-group-item">
               <div class="row">
@@ -12,6 +12,7 @@ const PostSummaryItem = (post) => {
                   <div class="fw-bolder pe-2">
                     ${title}
                   </div>
+                  ${tweets ? `<div class="text-secondary">${tweets} Tweets</div>` : ""}
                 </div>
                 <div class="col-2">
                   <img class="float-end rounded" src=${image} width="100px" height="100px"/>
